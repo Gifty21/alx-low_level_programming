03d19c12899a01527b7dca5c01609d3bc1d975f9
chinyere achoromadu <chinyereachoromadu44@gmail.com>
2022-06-08 19:46:41 -0700

    loop 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints a - z 10 times
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
