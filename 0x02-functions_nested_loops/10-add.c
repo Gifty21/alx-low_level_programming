0804a399b76b29e774f2b7b753d76c7cb1fcd5c0
chinyere achoromadu <chinyereachoromadu44@gmail.com>
2022-06-08 21:09:02 -0700

    wooo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints result of two numbers
+ * @num1: int of num1
+ * @num2: int of num2
+ * Return: Always 0
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
